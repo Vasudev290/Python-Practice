ad94a2947138fad914e3826fb2ff9648ff71c309
vasudev290 <vasukesavulu@gmail.com>
2024-08-19 12:22:42 +0530

    feat: datatypes

--- Practice/DataTypes/workingdatatype.py
+++ Practice/DataTypes/workingdatatype.py
@@ -1,8 +1,8 @@
 #how to represent int data type in python..!
 
 a= 4567        #Decimal Base(0-9)
-b= 0b111111    #Binary  Base (0 and 1) (0B or Ob)
-c= 0o43        #Ocatal Base (0-7) (0o or 0O)
+b= 0b011111    #Binary  Base (0 and 1) (0B or Ob)
+c= 0o437        #Ocatal Base (0-7) (0o or 0O)
 d= 0XABF       #Hexa Decial Base (0-9 and a-f or A-F)
 
 print(a)
