8cb11bc9b0d09ed1fd4c7e511faa6bee74043e31
vasudev290 <vasukesavulu@gmail.com>
2024-08-20 11:59:15 +0530

    feat: operator

---
+++ Thonny/Youtube/Ep-1.py
@@ -0,0 +1,13 @@
+#Print Statement
+
+'''print("2+3", end="=")
+print(2+3)'''
+
+print("Phone", "101", "Arjun", "Mouse","help", sep='...')
+print(76+ 56 * 3 -5 )
+
+print('Hii', 'Hii', sep='...', end='...')
+print("Hiii")
+
+#Python Sequences
+
