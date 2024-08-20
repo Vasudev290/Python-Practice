fa612e09e63139484ac74f05efbf71da1fc49933
vasudev290 <vasukesavulu@gmail.com>
2024-08-20 23:29:40 +0530

    feat: Added python sequences

---
+++ Thonny/Youtube/Ep-2.py
@@ -0,0 +1,32 @@
+#Sequences
+'''A sequence of character that,
+does not represent itself
+but is converted into another
+character which has a special meaning'''
+
+#\n (NextLine)
+print("Vasu\nDev")
+
+#\t (Tap Space)
+print("Will see \t\t Tomorrow")
+
+#\ (backslash)
+print('This is Shiva\'s Phone')
+print("he said,\"this is my phone\"")
+print("Oh my God, One day \"it'll happened soon\"")
+
+#\\ (Double-backslash)
+print("This is a Backslash: \\")
+
+#Py Trick
+print('He said, "this my phone"')
+print("This is 'Vivo Phone'")
+
+#Task
+'''print the using only one print statement"It's Hero Time!"
+                                                   -Ben 10'''
+
+
+print("'It's Hero Time!'N\n\t-Ben 10")
+
+
