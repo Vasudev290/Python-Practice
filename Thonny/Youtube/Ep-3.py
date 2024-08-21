4c1a38efd60a0b6eb11f027f66b925dd29ef0719
vasudev290 <vasukesavulu@gmail.com>
2024-08-21 16:28:37 +0530

    feat: datatype

---
+++ Thonny/Youtube/Ep-3.py
@@ -0,0 +1,54 @@
+#Variable
+
+name= 'Krishna'
+age= 100
+work = "future manager"
+crop= 'world'
+output= 'New World'
+hascreadit_card= True
+print(output)
+
+
+_= 'arjun'
+print(_)
+print(type(hascreadit_card))
+
+
+'''=============================='''
+
+Name = "Arjun"
+Age= 24
+Avail = True
+Rating= 4.8
+
+print(Name)
+print(type(Name))
+print(Age)
+type(type(Age))
+print(Avail)
+print(type(Avail))
+print(Rating)
+print(type(Rating))
+
+
+'''=================================='''
+
+
+Name, age, avail, rating = "vasu", 23, True, 5.0
+Name ="Arun"
+age = 20
+avail= False
+
+print( Name, age, avail)
+
+
+#Task
+#Bank Account
+name, balance = "Arjun", 500.00
+print(name, balance)
+print(type(balance))
+
+
+
+
+
