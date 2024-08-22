11a788a7d860389f8ed2579cffcee137cc30ca92
vasudev290 <vasukesavulu@gmail.com>
2024-08-22 10:45:33 +0530

    feat: operator

---
+++ Thonny/Youtube/Ep-4.py
@@ -0,0 +1,37 @@
+#Indexing Concept..!
+
+Name= ['Karna', 'Krishna', 'Arjuna', 'Bhimma','Abhimanyu']
+print(Name[0])
+print(type(Name))
+print(Name[1])
+print(Name[2])
+print(Name[3])
+print(Name[4])
+
+#Operators
+'''*Arithmatic [+, -, *, .\., %]
+   *Assignment
+   *Comparison
+   *Logical
+   *Identity
+   *Membership
+   *Bitwise'''
+
+#Arithmatic OP
+a=10
+b=5
+c=(a/b)
+print(c)
+print(type(c), c)
+print(a+50)
+print(a-5)
+print(a*50)
+
+#Task
+'''Find the output print(10+5*3)'''
+print(10+5*3)
+#Output - 25
+'''Find the output print(20-10/5*2)'''
+print(20-10/5*2)
+#Output - 16.0
+print(2**3**(2%10))
