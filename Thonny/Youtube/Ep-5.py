5db99d32313f1aca17b834db09bb9e36533d8265
vasudev290 <vasukesavulu@gmail.com>
2024-08-22 11:53:47 +0530

    feat: operator

---
+++ Thonny/Youtube/Ep-5.py
@@ -0,0 +1,43 @@
+#Comparison or Relational Op
+'''OPERATOR    DESCRIPTION  
+    >        Greater than   
+    >=        Greater or equal to
+    <        Less than      
+    <=        Less or equal to
+    ==        Equal to    
+    !=       Not equal to  '''
+
+
+a=20
+b=20
+print(a==b)
+
+a= 'Vasu'
+b= 'Vasud'
+
+#print(a==b)
+print(a<b)
+'''================================================'''
+
+#Assignment Operator (=, +=, -=, *=, %=)
+
+a=5
+a=a+3
+a=a*3
+a+= 5
+a-=4
+a*=2
+print(a)
+
+
+
+#Task
+#Bank Account
+name, balance= 'Vasu', 500.0
+print("Before interest", balance)
+
+#update the balance with 7% interest
+balance = (7/100 * balance) + balance
+print("After interest", balance)
+
+
