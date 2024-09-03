84e6a57a13e9638e08afa01f8dc52955b6cb5cb4
vasudev290 <vasukesavulu@gmail.com>
2024-09-03 14:30:41 +0530

    If-else

---
+++ YouTube/EMC/If-Else Statement-2/Exercise.py
@@ -0,0 +1,14 @@
+# 1. Get input for variable mark. if marks> 35 print pass else print fail
+""" mark=int(input('Enter your Marks :'))
+if(mark>35):
+    print("Pass")
+else:
+    print("fail") """
+
+
+# 2. Get input for variable income. if income is greater then 70000 schoolship is available, else not eligible for scholarship 
+income= int(input("Income :"))
+if(income > 7000):
+    print("Not Eligible for scholarship")
+else:
+    print("Scholarship is Available")
