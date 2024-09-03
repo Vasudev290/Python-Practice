4056f51046f5974f3e44ba5722a6487727d54240
vasudev290 <vasukesavulu@gmail.com>
2024-09-03 16:37:33 +0530

    if condition

--- YouTube/EMC/If-Else Statement-2/Exercise.py
+++ YouTube/EMC/If-Else Statement-2/Exercise.py
@@ -7,8 +7,25 @@ else:
 
 
 # 2. Get input for variable income. if income is greater then 70000 schoolship is available, else not eligible for scholarship 
-income= int(input("Income :"))
+""" income= int(input("Income :"))
 if(income > 7000):
     print("Not Eligible for scholarship")
 else:
-    print("Scholarship is Available")
+    print("Scholarship is Available") """
+
+# 3.Get input for a number and check whether it is divisible by both 3 and 5 or not if yes then print, 
+#the number is divisible by 3 and 5 else print the number is not divisible by 3 and 5 .?
+
+""" a= int(input("Enter the Value :"))
+if(a%3 == 0 and a%5 == 0):
+    print("Divisible by 3 and 5")
+else:
+    print("Not a Divisible by 3 and 5") """
+
+''' 4. Get input for a number and find it is even or odd'''
+
+a= int(input("Enter a Value :"))
+if(a%2 == 0):
+    print("It's a Even Number")
+else:
+    print("No it's Odd Number")
