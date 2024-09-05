923748748a81d1c78c0cbde50d83223b2b48c0ca
vasudev290 <vasukesavulu@gmail.com>
2024-09-05 17:13:03 +0530

    for loop

---
+++ YouTube/EMC/Forloop/Assign1.py
@@ -0,0 +1,19 @@
+# 6. Write a program to complete the sum of the first 5 natural numbers 
+add=0
+for i in range(1,6):
+    add=add+i
+print(add)
+
+# 7. write a program to read 10 numbers from the keyboard and find their sum  and average
+
+a=[]
+print("Enter 10 Number :")
+for i in range(10):
+    num=int(input("Enter num :" ))
+    a.append(num)
+print(a)
+
+sum=0
+for i in a:
+    sum=sum+i
+print(sum)
