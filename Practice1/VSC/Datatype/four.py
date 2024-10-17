c855bc39166584ffb2c9c1a0206da9f1de54d94e
Vasudev290 <vasukesavulu@gmail.com>
2024-10-17 22:49:35 +0530

    Bool-Datatype

---
+++ Practice1/VSC/Datatype/four.py
@@ -0,0 +1,15 @@
+#How to represent bool values in datatypes
+print(bool(True))
+print(bool(False))
+print(bool(100))
+print(bool(0))
+
+
+
+print(bool(100)) #True
+print(bool(0)) #False
+print(bool('True')) #True
+print(bool('False'))#True
+print(bool(454.90))#True
+print(bool(00.00))#False
+print(bool(00.01))#True
