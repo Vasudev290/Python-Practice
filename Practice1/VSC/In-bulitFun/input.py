781a16b2008ab92ba568b0853091c6dfcdf3e10e
Vasudev290 <vasukesavulu@gmail.com>
2024-10-18 15:19:00 +0530

    operators

--- Practice1/VSC/In-bulitFun/input.py
+++ Practice1/VSC/In-bulitFun/input.py
@@ -1,22 +1,23 @@
 #Read 2 numbers and print min and max value
-""" num1= input('Enter 1 value:')
+num1= input('Enter 1 value:')
 num2= input('Enter 2 value:')
 if num1 > num2 :
     print('Max value is :', num1)
 else:
-    print('Min value is :', num2) """
+    print('Min value is :', num2)
 
 
-
-""" num1= int(input('Enter 1 value:'))
+num1= int(input('Enter 1 value:'))
 num2= int(input('Enter 2 value:'))
 if num1 > num2 :
     print('Max value is :', num1)
 else:
-    print('Min value is :', num2) """
+    print('Min value is :', num2)
 
-""" num1= int(input('Enter 1 value:'))
+
+num1= int(input('Enter 1 value:'))
 num2= int(input('Enter 2 value:'))
-print(num1+num2) """
+print(num1+num2)
+
 
 print("Sum :",int(input('Enter First Value :'))+int(input('Enter second value :')))
