9134cabe10cab5472c88f742f442bcbfb462ce47
Vasudev290 <vasukesavulu@gmail.com>
2024-11-08 14:26:22 +0530

    function

---
+++ YouTube/Swaroop/Function/one.py
@@ -0,0 +1,10 @@
+#Finding the sum of two Numbers
+
+a = int(input("Enter the A Value :"))
+b = int(input("Enter the B Value :"))
+
+def add_Numbers(a,b):
+    print(f"Adding A + B Value :{a+b}")
+    
+    
+add_Numbers(a,b)
